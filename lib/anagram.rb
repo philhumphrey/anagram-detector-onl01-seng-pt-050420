ae8f54bd5136cf26b9465fe107abe98dc371505e
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-31 17:13:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
     @word = word
     
     def match(word_array)
-      word_array.select do |words|
-        
+      word_array.select do |word|
+        word.split
       
       
     end
