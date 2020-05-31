85b79e0c4716e4c897972f289c514a10b2e66234
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-31 17:16:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
     
     def match(word_array)
       word_array.select do |word|
-        word.split == @word.split
+        word.split.sort == @word.split.sort
       end
   end
 end
